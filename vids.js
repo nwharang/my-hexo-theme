64f6d8602052f4a45422a2e9acafb25b25fbec31
nootifmemory <nootifmemory@users.noreply.github.com>
2021-04-13 08:26:49 +0000

    deploy: d97a0417b0d710c22991f1810d5ebff67b4640e9

--- vids.js
+++ vids.js
@@ -21,15 +21,82 @@ function execute() {
             let number = 1
             const { result } = response ;
             result.items.forEach(item => {
-                console.log(`Title: ${item.snippet.title}\nDescription: ${item.snippet.description}\nID: ${item.id.videoId}`)
                 output.innerHTML +=  `         
-                <div>
-                <iframe class="vid${number}" width="100%" height="600" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <div class="wrapper">
+                <iframe id="vid${number}" width="100%" height="600" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <style>
+                /*Floating CSS Start*/
+.wrapper{
+  width:100%;
+  margin:0 auto;
+}
+        @keyframes fade-in-up {
+            0% {
+                opacity: 0;
+            }
+            100% {
+                transform: translateY(0);
+                opacity: 1;
+            }
+        }
+
+        .stuck {
+            position: fixed;
+            bottom: -10px;
+            right: 50px;
+            transform: translateY(100%);
+            width: 460px;
+            height: 300px;
+            animation: fade-in-up .25s ease forwards;
+            z-index: 999;
+        }
+
+        /*Floating CSS End*/
+
+@keyframes example {
+    0%   {background-color: red;}
+    25%  {background-color: #ff7037;}
+    50%  {background-color: red;}
+    100% {background-color: #ff7037;}
+}
+
+                </style>
                 </div>
                 `;
                 number++
             });
+/*Floating Code for Iframe Start*/
+if (jQuery('iframe[src*="https://www.youtube.com/embed/"],iframe[src*="https://player.vimeo.com/"],iframe[src*="https://player.vimeo.com/"]').length > 0) {
+  /*Wrap (all code inside div) all vedio code inside div*/
+  jQuery('iframe[src*="https://www.youtube.com/embed/"],iframe[src*="https://player.vimeo.com/"]').wrap("<div class='iframe-parent-class'></div>");
+  /*main code of each (particular) vedio*/
+  jQuery('iframe[src*="https://www.youtube.com/embed/"],iframe[src*="https://player.vimeo.com/"]').each(function(index) {
+
+      /*Floating js Start*/
+      var windows = jQuery(window);
+      var iframeWrap = jQuery(this).parent();
+      var iframe = jQuery(this);
+      var iframeHeight = iframe.outerHeight();
+      var iframeElement = iframe.get(0);
+      windows.on('scroll', function() {
+          var windowScrollTop = windows.scrollTop();
+          var iframeBottom = iframeHeight + iframeWrap.offset().top;
+          //alert(iframeBottom);
+
+          if ((windowScrollTop > iframeBottom)) {
+              iframeWrap.height(iframeHeight);
+              iframe.addClass('stuck');
+              jQuery(".scrolldown").css({"display": "none"});
+          } else {
+              iframeWrap.height('auto');
+              iframe.removeClass('stuck');
+          }
+      });
+      /*Floating js End*/
+  });
+}
     });
   }
 
-document.getElementsByClassName('vids')[0].requestPictureInPicture();
+
+/*Floating Code for Iframe End*/
