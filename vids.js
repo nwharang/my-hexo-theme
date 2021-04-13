b6382428e9491a0bf3199eac48e879b01bc8df87
nootifmemory <nootifmemory@users.noreply.github.com>
2021-04-13 10:55:09 +0000

    deploy: da0ffdea0bf1398b90c56ca831acf55d62d7decc

--- vids.js
+++ vids.js
@@ -1,6 +1,6 @@
 gapi.load("client", loadClient);
 function loadClient() {
-    gapi.client.setApiKey( "$env:SUPER_SECRET" );
+    gapi.client.setApiKey( "$env:API_KEY" );
     return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
         .then(function() { 
             execute();
