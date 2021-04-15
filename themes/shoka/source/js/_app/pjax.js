80031917e2e76d61eb698fcf1203e5aebf9326b9
nootifmemory <nootifhuhong@gmail.com>
2021-04-15 21:28:21 +0700

    a

--- themes/shoka/source/js/_app/pjax.js
+++ themes/shoka/source/js/_app/pjax.js
@@ -55,21 +55,7 @@ const siteRefresh = function (reload) {
   vendorJs('copy_tex');
   vendorCss('mermaid');
   vendorJs('chart');
-  vendorJs('valine', function() {
-    var options = Object.assign({}, CONFIG.valine);
-    options = Object.assign(options, LOCAL.valine||{});
-    options.el = '#comments';
-    options.pathname = LOCAL.path;
-    options.pjax = pjax;
-    options.lazyload = lazyload;
 
-    new MiniValine(options);
-
-    setTimeout(function(){
-      positionInit(1);
-      postFancybox('.v');
-    }, 1000);
-  }, window.MiniValine);
 
   if(!reload) {
     $.each('script[data-pjax]', pjaxScript);
