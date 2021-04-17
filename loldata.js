39dbedb8ed586175cfd658dbab10ec0ae7075f70
nootifmemory <nootifmemory@users.noreply.github.com>
2021-04-17 03:33:55 +0000

    deploy: 5abdf137042b27717a04e4ad79121fa68f7b56e4

--- loldata.js
+++ loldata.js
@@ -1,5 +1,6 @@
-function fetchdata () {
-  fetch("https://thingproxy.freeboard.io/fetch/https://acs-garena.leagueoflegends.com/v1/stats/player_history/VN/486211408?begIndex=0&endIndex=1", {
+async function fetchdata () {
+
+  let response = await fetch("https://thingproxy.freeboard.io/fetch/https://acs-garena.leagueoflegends.com/v1/stats/player_history/VN/486211408?begIndex=0&endIndex=1", {
   "headers": {
     "accept": "application/json, text/javascript, */*; q=0.01",
     "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
@@ -15,9 +16,19 @@ function fetchdata () {
   "body": null,
   "method": "GET",
   "mode": "cors",
-  "credentials": "include"
-}).then(response => {
- console.log(response);
-})};
+  "credentials": "include",
+});
+let data = await response.json()
+console.log(data.games.games[0].participantIdentities[0].player.summonerName)
+console.log(data.games.games[0].participants[0].championId)
+}
 
+async function champion () {
+  let response = await fetch("https://ddragon.leagueoflegends.com/cdn/11.8.1/data/vn_VN/champion.json")
+  let data = await response.json()
+  let  championid  = data.data
+  console.log(data)
+  console.log(championid)
+}
 fetchdata();
+champion();
