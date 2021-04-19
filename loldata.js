f6a5254b8f66f2335692fe2d1c3afcaeb4936da8
nootifmemory <nootifmemory@users.noreply.github.com>
2021-04-19 10:38:22 +0000

    deploy: c5facecac6c80a222511493d2f78481959040422

--- loldata.js
+++ loldata.js
@@ -8,7 +8,7 @@ var keys = [];
 var output = document.getElementsByClassName("md body")[0];
 async function fetchdata () {
   let accId = '486211408';
-  let link = 'https://cors-anywhere.herokuapp.com/https://acs-garena.leagueoflegends.com/v1/stats/player_history/VN/' + await accId + '?';
+  let link = 'https://obscure-woodland-31326.herokuapp.com/fetch/https://acs-garena.leagueoflegends.com/v1/stats/player_history/VN/' + await accId + '?';
       if(champId != null)
         link += 'champion=' +champId+'&';
       if(queue != null)
