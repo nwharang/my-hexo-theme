97c35c507ff3fc4b187d4e3ded877c55d2415561
NooTifmemory <nootifhuhong@gmail.com>
2021-05-22 13:39:35 +0700

    a

--- source/_posts/loldata.md
+++ source/_posts/loldata.md
@@ -5,3 +5,4 @@ tags:
 ---
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch-jsonp/1.0.6/fetch-jsonp.min.js"></script>
 <script src="/loldata.js"></script>
+
--- source/loldata.js
+++ source/loldata.js
@@ -1,13 +1,11 @@
 const sp = '%20';
 const champId = null;
 const queue = null;
-const endIndex = 1 ;
+const endIndex = 10 ;
 const begIndex = 0 ;
-var champlist = [];
-var champkey = [];
-var keys = [];
-
-async function fetchdata () {
+const summonerName = undefined ;
+const championId = ``;
+const fetchdata = async function () {
   let accId = '486211408';
   let link = 'https://thingproxy.freeboard.io/fetch/https://acs-garena.leagueoflegends.com/v1/stats/player_history/VN/' + await accId + '?';
       if(champId != null)
@@ -37,22 +35,34 @@ async function fetchdata () {
   "credentials": "include",
 });
 let data = await response.json()
-let summonerName = data.games.games[0].participantIdentities[0].player.summonerName;
-let championId = data.games.games[0].participants[0].championId;
-champion();
-var myJSON = JSON.stringify(champkey);
+var output = document.getElementsByClassName("md body")[0]
+var summonerName = data.games.games[0].participantIdentities[0].player.summonerName;
+console.log(data)
+console.log(await champion (data.games.games[9].participants[0].championId));
+console.log(await champion (data.games.games[8].participants[0].championId));
+console.log(await champion (data.games.games[7].participants[0].championId));
+console.log(await champion (data.games.games[6].participants[0].championId));
+console.log(await champion (data.games.games[5].participants[0].championId));
+console.log(await champion (data.games.games[4].participants[0].championId));
+console.log(await champion (data.games.games[3].participants[0].championId));
+console.log(await champion (data.games.games[2].participants[0].championId));
+console.log(await champion (data.games.games[1].participants[0].championId));
+console.log(await champion (data.games.games[0].participants[0].championId));
+
+};
+
+async function champion (championId) {
+
+  let response = await fetch("https://ddragon.leagueoflegends.com/cdn/11.8.1/data/vn_VN/champion.json")
+  let { data } = await response.json()
+  for  (var i = 0 in data ) {
+    if ( data[i].key != championId ){
+      continue;
+    } else {
+      return data[i].id
+    }
+  }
 }
 
-async function champion () {
-  let response = await fetch("https://ddragon.leagueoflegends.com/cdn/11.8.1/data/vn_VN/champion.json")
-  let {data} = (await response.json())
-  var dataID = Object.entries(data)
 
-  dataID.forEach((s) => {
-    //console.log(`Champions: ${s[0]} is key : ${s[1].key}`)
-    var newLength  = champkey.push(parseInt(s[1].key))
-    var newLength  = champlist.push(s[0])
-  });
-};
-fetchdata(); 
-
+fetchdata();
